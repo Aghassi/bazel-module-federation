57b0776920c8a182125fa8e1af49ac4f7725b3af
David <david.aghassi@gmail.com>
2021-11-05 04:49:23 +0000

    chore: update generate-route-manifest to actually generate
    Looks through interim artifacts and maps routes to the
    remoteEntry

--- bazel/js/internals/routes/generate-route-manifest.js
+++ bazel/js/internals/routes/generate-route-manifest.js
@@ -6,18 +6,37 @@ const manifest = {
   "/": "index.sha.js",
 };
 
+const getDirectories = (source) =>
+  fs
+    .readdirSync(source, { withFileTypes: true })
+    .filter((dirent) => dirent.isDirectory())
+    .map((dirent) => dirent.name);
+const getFilesInDirectory = (source) =>
+  fs
+    .readdirSync(source, { withFileTypes: true })
+    .filter((dirent) => dirent.isFile())
+    .map((dirent) => dirent.name);
+
+const routeDirs = getDirectories(
+  "bazel-out/k8-fastbuild/bin/src/client/routes/routes"
+);
+
 // For each folder we read, check what it is and
 // determine the route mapping
-// for (folder in folders) {
-//   switch (folder) {
-//     case "default":
-//       manifest["/"] = "index.sha.js";
-//       break;
-//     default:
-//       manifest[`${key}`] = "key.sha.js";
-//       break;
-//   }
-// }
+routeDirs.forEach((dir) => {
+  const remoteEntry = getFilesInDirectory(
+    `bazel-out/k8-fastbuild/bin/src/client/routes/routes/${dir}`
+  ).filter((file) => !file.endsWith(".map") && file.startsWith("remote"));
+
+  switch (dir) {
+    case "default":
+      manifest["/"] = `${remoteEntry}`;
+      break;
+    default:
+      manifest[`${dir}`] = `${remoteEntry}`;
+      break;
+  }
+});
 
 // Finally write out the manifest
 fs.writeFileSync(
