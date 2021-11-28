31e54b47655828c0a93b9a951350b1d0eec73468
David Aghassi <3680126+Aghassi@users.noreply.github.com>
2021-11-28 12:34:11 -0500

    chore: fix cross architecture compilation
    Fixes rules_docker not working without rules_go on macOS
    Adds check for architecture to manifest script to support M1

--- bazel/js/internals/routes/generate-route-manifest.js
+++ bazel/js/internals/routes/generate-route-manifest.js
@@ -17,18 +17,18 @@ const getFilesInDirectory = (source) =>
     .filter((dirent) => dirent.isFile())
     .map((dirent) => dirent.name);
 
-const platform = process.platform === "darwin" ? process.platform : 'k8'
-const outputRoutesBase = `bazel-out/${platform}-fastbuild/bin/src/client/routes/routes`
-
+const platform = process.platform === "darwin" ? process.platform : "k8";
+const architecture = process.arch === "arm64" ? "_arm64" : "";
+const outputRoutesBase = `bazel-out/${platform}${architecture}-fastbuild/bin/src/client/routes/routes`;
 
 const routeDirs = getDirectories(outputRoutesBase);
 
 // For each folder we read, check what it is and
 // determine the route mapping
 routeDirs.forEach((dir) => {
-  const remoteEntry = getFilesInDirectory(
-    `${outputRoutesBase}/${dir}`
-  ).filter((file) => !file.endsWith(".map") && file.startsWith("remote"));
+  const remoteEntry = getFilesInDirectory(`${outputRoutesBase}/${dir}`).filter(
+    (file) => !file.endsWith(".map") && file.startsWith("remote")
+  );
 
   switch (dir) {
     case "default":
