976e369ef0927d0778db85bcb000ea0636f856d6
lshadler <lucas_shadler@intuit.com>
2021-12-01 20:22:43 -0800

    feat: progress

--- src/server/server.js
+++ src/server/server.js
@@ -3,13 +3,13 @@
 const getPageTemplate = require("./getPageTemplate");
 
 const http = require("http");
-
 /**
  *
  * @param {import('http').ClientRequest} req request
  * @param {import('http').ServerResponse} res response
  */
 const requestListener = function (req, res) {
+  console.log("hit");
   res.writeHead(200);
   res.end(
     getPageTemplate({
