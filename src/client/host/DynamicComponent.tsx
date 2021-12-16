dab5e9c0703873b016ca9ff18be6b83301c2775f
David Aghassi <david_aghassi@intuit.com>
2021-12-15 21:37:48 -0800

    fixed dynamic react router

--- src/client/host/DynamicComponent.tsx
+++ src/client/host/DynamicComponent.tsx
@@ -35,11 +35,13 @@ const useDynamicScript = (args) => {
 
     element.onload = () => {
       console.log(`Dynamic Script Loaded: ${args.url}`);
+      // This line does not update the state and rerender
       setReady(true);
     };
 
     element.onerror = () => {
       console.error(`Dynamic Script Error: ${args.url}`);
+      // These lines do update the state...
       setReady(false);
       setFailed(true);
     };
@@ -59,22 +61,20 @@ const useDynamicScript = (args) => {
 };
 
 export default function DynamicComponent(props) {
-  if (!window[props.system.scope]) {
-    const { ready, failed } = useDynamicScript({
-      url: props.system && props.system.url,
-    });
+  const { ready, failed } = useDynamicScript({
+    url: props.system && props.system.url,
+  });
 
-    if (!props.system) {
-      return <h2>Not system specified</h2>;
-    }
+  if (!props.system) {
+    return <h2>Not system specified</h2>;
+  }
 
-    if (!ready) {
-      return <h2>Loading dynamic script: {props.system.url}</h2>;
-    }
+  if (!ready) {
+    return <h2>Loading dynamic script: {props.system.url}</h2>;
+  }
 
-    if (failed) {
-      return <h2>Failed to load dynamic script: {props.system.url}</h2>;
-    }
+  if (failed) {
+    return <h2>Failed to load dynamic script: {props.system.url}</h2>;
   }
 
   const Component = React.lazy(
