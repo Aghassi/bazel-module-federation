30407a5b932a9b2e9958b9ba653a7f743a6565bb
lshadler <lucas_shadler@intuit.com>
2021-12-15 21:38:03 -0800

    fix: react router dynamic script

--- src/client/host/DynamicComponent.tsx
+++ src/client/host/DynamicComponent.tsx
@@ -61,22 +61,20 @@ const useDynamicScript = (args) => {
 };
 
 export default function DynamicComponent(props) {
-  if (!window[props.system.scope]) {
-    const { ready, failed } = useDynamicScript({
-      url: props.system && props.system.url,
-    });
+  const { ready, failed } = useDynamicScript({
+    url: props.system && props.system.url,
+  });
 
-    if (!props.system) {
-      return <h2>No system specified</h2>;
-    }
+  if (!props.system) {
+    return <h2>No system specified</h2>;
+  }
 
-    if (!ready) {
-      return <h2>Loading dynamic script: {props.system.url}</h2>;
-    }
+  if (!ready) {
+    return <h2>Loading dynamic script: {props.system.url}</h2>;
+  }
 
-    if (failed) {
-      return <h2>Failed to load dynamic script: {props.system.url}</h2>;
-    }
+  if (failed) {
+    return <h2>Failed to load dynamic script: {props.system.url}</h2>;
   }
 
   const Component = React.lazy(
